d40ec70b4e4bde1a0bc33d799801db2791236616
Trunk <Jesdakorns@gmail.com>
2023-11-09 23:22:52 +0700

    fix middleware

--- middleware.ts
+++ middleware.ts
@@ -3,16 +3,15 @@ import { NextRequest, NextResponse } from "next/server";
 import { withAuth } from "next-auth/middleware"
 
 export async function middleware(req: NextRequest) {
-  // const token = await getToken({
-  //   req,
-  //   secret: process?.env?.NEXT_PUBLIC_NEXTAUTH_SECRET,
-  //   raw: true,
-  // });
-  // console.log(`🚀 ~ file: middleware.ts ~ line 11 ~ middleware ~ token`, token)
-
-  // if (!token) {
-  //   return NextResponse.redirect(new URL('/signin', req.url));
-  // }
+  const token = await getToken({
+    req,
+    secret: process?.env?.NEXT_PUBLIC_NEXTAUTH_SECRET,
+    raw: true,
+  });
+  const { pathname } = req.nextUrl
+  if (!token && pathname !== '/signin') {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
   // return NextResponse.next();
 }
 
