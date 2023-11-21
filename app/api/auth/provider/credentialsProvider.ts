0dd6dc5999291985816e4ea864d80ec1336540f7
Trunk <Jesdakorns@gmail.com>
2023-11-22 02:55:34 +0700

    fix login error

--- app/api/auth/provider/credentialsProvider.ts
+++ app/api/auth/provider/credentialsProvider.ts
@@ -11,29 +11,12 @@ export const credentialsProvider = CredentialsProvider({
             email: string,
             password: string,
         };
-        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 12 ~ authorize ~ email, password`, email, password)
-        // const res = await postSignIn({ email, password })
-        // console.log(`🚀 ~ file: credentialsProvider.ts ~ line 17 ~ authorize ~ res`, res)
-        // log('Credentials:', res);
-        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 23 ~ authorize ~ process.env.NEXT_PUBLIC_API_BASE_URL`, process.env.NEXT_PUBLIC_API_BASE_URL)
-        const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/auth/login`, {
-            method: "POST",
-            body: JSON.stringify({ email, password }),
-            headers: { "Content-Type": "application/json" },
-        }).then((response) => response.json());
-        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 23 ~ authorize ~ res`, res)
         try {
-
-            const isLogin = true
-            if (!isLogin) return null
-            // if (!res.data) return null
+            const res = await postSignIn({ email, password })
+            if (!res.data) return null
 
             const profile = {
-                // ...res.data,
-                id: '1',
-                name: 'dddd',
-                email: 'dddd@gmail.com',
-                image: ''
+                ...res.data,
             }
 
             return profile;
