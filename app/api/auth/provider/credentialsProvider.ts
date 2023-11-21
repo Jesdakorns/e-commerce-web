7fd3ea3105e805e5549abbec72e8085c94a1a2ef
Trunk <Jesdakorns@gmail.com>
2023-11-22 01:45:55 +0700

    test post api loing

--- app/api/auth/provider/credentialsProvider.ts
+++ app/api/auth/provider/credentialsProvider.ts
@@ -5,13 +5,13 @@ export const credentialsProvider = CredentialsProvider({
     name: "credentials",
     credentials: {},
     async authorize(credentials) {
+        const { email, password } = credentials as {
+            email: string,
+            password: string,
+        };
+        const res = await postSignIn({ email, password })
+        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 14 ~ authorize ~ res`, res)
         try {
-            const { email, password } = credentials as {
-                email: string,
-                password: string,
-            };
-            const res = await postSignIn({ email, password })
-            console.log(`🚀 ~ file: credentialsProvider.ts ~ line 14 ~ authorize ~ res`, res)
 
             // const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/auth/login?mode=${mode}`, {
             //     method: "POST",
@@ -25,7 +25,7 @@ export const credentialsProvider = CredentialsProvider({
                 ...res.data,
             }
 
-            return profile;
+            return null;
         } catch (error) {
             return null;
         }
