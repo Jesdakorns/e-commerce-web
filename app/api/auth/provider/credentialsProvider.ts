9915d2fb82b62679ad2d44daf975a51302c6c5e5
Trunk <Jesdakorns@gmail.com>
2023-11-22 02:27:21 +0700

    no message

--- app/api/auth/provider/credentialsProvider.ts
+++ app/api/auth/provider/credentialsProvider.ts
@@ -12,16 +12,18 @@ export const credentialsProvider = CredentialsProvider({
             password: string,
         };
         console.log(`🚀 ~ file: credentialsProvider.ts ~ line 12 ~ authorize ~ email, password`, email, password)
-        const res = await postSignIn({ email, password })
-        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 17 ~ authorize ~ res`, res)
-        log('Credentials:', res);
+        // const res = await postSignIn({ email, password })
+        // console.log(`🚀 ~ file: credentialsProvider.ts ~ line 17 ~ authorize ~ res`, res)
+        // log('Credentials:', res);
+        const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/auth/login`, {
+            method: "POST",
+            body: JSON.stringify({ email, password }),
+            headers: { "Content-Type": "application/json" },
+        }).then((response) => response.json());
+        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 23 ~ authorize ~ process.env.NEXT_PUBLIC_API_BASE_URL`, process.env.NEXT_PUBLIC_API_BASE_URL)
+        console.log(`🚀 ~ file: credentialsProvider.ts ~ line 23 ~ authorize ~ res`, res)
         try {
 
-            // const res = await fetch(process.env.NEXT_PUBLIC_API_BASE_URL + `/auth/login?mode=${mode}`, {
-            //     method: "POST",
-            //     body: JSON.stringify({ email, password }),
-            //     headers: { "Content-Type": "application/json" },
-            // });
 
             if (!res.data) return null
 
