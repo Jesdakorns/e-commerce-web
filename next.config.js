a7d87d60d279c0f5c18942a2ece4cb3939ff7c86
Trunk <Jesdakorns@gmail.com>
2023-11-29 15:02:08 +0700

    fix pwa

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
 /** @type {import('next').NextConfig} */
 const runtimeCaching = require("next-pwa/cache");
 const withPWA = require("next-pwa")({
-    disable: process.env.NODE_ENV === "development",
+    disable:process.env.NODE_ENV === "development",
     dest: "public",
     register: true,
-    skipWaiting: false,
+    skipWaiting: true,
     runtimeCaching,
-    poweredByHeader: false,
-    async headers() {
-        return [
-            {
-                source: '/',
-                headers: [
-                    {
-                        key: 'Content-Security-Policy',
-                        value: "frame-ancestors 'none'",
-                    },
-                ],
-            },
-        ];
-    },
 });
 const nextConfig = withPWA({});
 
